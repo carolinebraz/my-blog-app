5732d5f260046ba14f796af6bfba67c8709d2ea1
carolinebraz <jcarolinebraz@gmail.com>
2023-07-26 22:01:52 -0300

    Cria rotas de navegação entre as páginas home e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
