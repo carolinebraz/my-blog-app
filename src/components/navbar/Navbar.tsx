708b893be7cbc46e508888885c63573015e2c189
carolinebraz <jcarolinebraz@gmail.com>
2023-07-27 15:37:22 -0300

    Adiciona rotas Login e Home no componente Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
@@ -6,6 +8,8 @@ function Navbar() {
                     <div className='text-2xl font-bold uppercase'>My Blog</div>
 
                     <div className='flex gap-4'>
+                        <Link to='/login' className='hover:underline'>Login</Link>
+                        <Link to='/home' className='hover:underline'>Home</Link>
                         <div className='hover:underline'>Posts</div>
                         <div className='hover:underline'>Topics</div>
                         <div className='hover:underline'>Add topics</div>
